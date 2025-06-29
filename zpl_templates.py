a9207a8b54a78e04612ca154756a8f216209a0e9
Jimmy Henderson <65567808+encondata@users.noreply.github.com>
2025-06-28 20:02:51 -0500

    ZPL Updates

--- zpl_templates.py
+++ zpl_templates.py
@@ -15,7 +15,44 @@ Vegas_Crate = """
 """
 
 Vegas_Front = """
-# TODO: ZPL template for Vegas Front
+            ^XA
+            
+            ^FX Set Label Size
+            ^PW406^LL203
+            
+            ^FO10,10^A@N,35,,E:85620388.TTF^BCN,35,Y,N,N,A^A@N,35,,E:85620388.TTF^FD{row['asset id']}^FS
+
+            ^FX Target Line
+            ^FT10,110^A@N,35,,E:85620388.TTF^FDTarget:^FS
+            ^FT145,110^A@N,35,^FD{row['asset track']}^FS
+            ^FT320,110^A@N,35,^FDRU:^FS
+            ^FT385,110^A@N,35,^FD{row['asset tpos']}^FS
+                            
+                            
+            ^FX Solid Dividing Line
+            ^FO10,120^GB590,5,3,B,8^FS
+                            
+            ^FX Server Information
+            ^FT10,155^A@N,35,^FDName:^FS
+            ^FT120,155^A@N,35,^FD{row['asset name']}^FS
+            ^FT10,190^A@N,35,^FDSerial:^FS
+            ^FT120,190^A@N,35,^FD{row['asset serial num']}^FS
+            ^FT10,225^A@N,35,^FDModel:^FS
+            ^FT120,225^A@N,35,^FD{row['asset model']}^FS
+            ^FX Source Line
+            ^FT10,280^A@N,35,^FDSource:^FS
+            ^FT130,280^A@N,35,^FD{row['asset srack']}^FS
+            ^FT320,280^A@N,35,^FDRU:^FS
+            ^FT385,280^A@N,35,^FD{row['asset spos']}^FS
+                            
+            ^FX Draw QR Code and Move Info
+            ^FT480,280^A0B,36,20^FD{row['source site']}^FS
+            ^FT480,180^A0N,36,20^FD{row['move date']}^FS
+            ^FT485,285^BY1,2.0,5^BQN,2,4,Q,7^FDM<{row['asset id']}^FS
+            
+            ^FX PQ is number of labels
+            ^PQ1,0,1,Y
+            ^XZ
 """
 
 Vegas_All = """
